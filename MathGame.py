aa030f746560c7b5ee182c4f58f67707a3f74615
peter-okafor <hotshot_peter@yahoo.com>
2018-08-29 18:12:47 +0100

    added reiteration

--- MathGame.py
+++ MathGame.py
@@ -17,7 +17,7 @@ class addition:
 			ans=int(ans)
 
 			if ans!=no3:
-				print('olodo')
+				print('wrong')
 				Wrong=Wrong+1
 				no3=str(no3)
 				print('The answer is '+no3)
@@ -27,7 +27,7 @@ class addition:
 			TimeTaken=TimeTaken+1
 
 		if Right==0:
-			print('How much more dumb can you get? Mtchew!')
+			print('You got it all wrong. But you can do better.')
 
 		if Right==5:
 			print('You are a GENIUS! You got it all RIGHT!')
@@ -38,62 +38,41 @@ class addition:
 			print('You were right '+Right+' times')
 			print('You were wrong '+Wrong+' times')
 
-		exit("done")
 class multiplication:
 	def __init__(self):
-		order=0
-		while order<=1:
-			Right, Wrong=0, 0
-			
-			TimeTaken=0
-			while TimeTaken<5:
-				no1=random.randint(1,20)
-				no2=random.randint(1,20)
-				no3=no1*no2
-				no1=str(no1)
-				no2=str(no2)
+		Right, Wrong=0, 0
+		
+		TimeTaken=0
+		while TimeTaken<5:
+			no1=random.randint(1,20)
+			no2=random.randint(1,20)
+			no3=no1*no2
+			no1=str(no1)
+			no2=str(no2)
 
-				print('what is '+no1+'*'+no2)
-				ans=input()
-				ans=int(ans)
+			print('what is '+no1+'*'+no2)
+			ans=input()
+			ans=int(ans)
 
-				if ans!=no3:
-					print('olodo')
-					Wrong=Wrong+1
-					no3=str(no3)
-					print('The answer is '+no3)
-					print()
+			if ans!=no3:
+				print('wrong')
+				Wrong=Wrong+1
+				no3=str(no3)
+				print('The answer is '+no3)
 
-				if ans==no3:
-					print('''correct''')
-					Right=Right+1
-				TimeTaken=TimeTaken+1
+			if ans==no3:
+				print('''correct''')
+				Right=Right+1
+			TimeTaken=TimeTaken+1
 
-			if Right==0:
-				print('How much more dumb can you get? Mtchew!')
+		if Right==0:
+			print('You got it all wrong. But you can do better.')
 
-			if Right==5:
-				print('You are a GENIUS! You got it all RIGHT!')
+		if Right==5:
+			print('You are a GENIUS! You got it all RIGHT!')
 
-			if Right<5:
-				Right=str(Right)
-				Wrong=str(Wrong)
-				print('You were right '+Right+' times')
-				print('You were wrong '+Wrong+' times')
-
-			print('''Do you want to play again?''')
-			print('yes or no')
-			response=input()
-			response=str(response)
-			a='yes'
-			b='no'
-			if response==b or response!=a:
-				print('bye')
-				order=2
-
-			if response==a:
-				print('ok')
-				order=1
-			
-
-		input()
+		if Right<5:
+			Right=str(Right)
+			Wrong=str(Wrong)
+			print('You were right '+Right+' times')
+			print('You were wrong '+Wrong+' times')
--- Selector.py
+++ Selector.py
@@ -1,7 +1,21 @@
-ans = input("select a game. \n a) Addition b) Multiplication\n")
-ans = str(ans)
 from MathGame import *
-if ans=="a":
-    addition()
-if ans=="b":
-    multiplication()
+goagain = 1
+while goagain == 1:
+	ans = input("select a game by entering the number 1 or 2. \n 1) Addition 2) Multiplication\n")
+	ans = int(ans)
+	if ans==1:
+		addition()
+	if ans==2:
+		multiplication()
+	
+	print('''Do you want to play again?''')
+	print('1) yes 2) no')
+	response=input()
+	response=int(response)
+	
+	if response==2 or response!=1:
+		print('bye')
+		goagain=0
+	if response==1:
+		print('ok')
+		goagain=1
